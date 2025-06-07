cfbc5db9a691b114780bee1067ab67d4eb5b58d4
Ramid Khan <ramidzkh@gmail.com>
2025-06-07 21:49:51 +1000

    Split ALU out of processor.vhd

--- ghdl.sh
+++ ghdl.sh
@@ -3,6 +3,6 @@
 set -ex
 
 rm work-obj08.cf || true
-ghdl -a --std=08 -Wall src/processor.vhd src/registers.vhd sim/testbench.vhd
+ghdl -a --std=08 -Wall src/processor.vhd src/alu.vhd src/registers.vhd sim/testbench.vhd
 ghdl -e --std=08 -Wall testbench
 ghdl -r --std=08 -Wall testbench --fst=wave.fst --stop-time=6500ns
